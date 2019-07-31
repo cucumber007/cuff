2f50ec97e6c57638b4cad0c206b14fe3d285ff2d
spqrta <spqrta@gmail.com>
2019-07-31 16:17:07 +0300

    Features

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import json
+import matplotlib.pyplot as plt
 
 #git log --abbrev-commit --shortstat --graph --date=short > log.txt
 
@@ -12,7 +13,7 @@ with open("git.log", "r") as f:
 
 	for index, line in enumerate(f.readlines()):
 		#print(line[0])
-		if line[0] == "*":
+		if "*" in line[0]:
 			if current_commit:
 				commits.append(current_commit)
 			current_commit = {
@@ -50,8 +51,7 @@ with open("git.log", "r") as f:
 	#print(json.dumps(commits, indent=4))
 
 stats = {}
-sum_changes = 0
-sum_norm = 0
+
 
 for index, commit in enumerate(commits):
 	author = commit["author"]
@@ -64,20 +64,40 @@ for index, commit in enumerate(commits):
 			"lines_changed": 0,
 			"insertions": 0,
 			"deletions": 0,
-			"normalized_changes": 0,
-			"avg_changes": 0,
-			"avg_normalized_changes": 0,
+			"normalized_changes": 0
 		}
 	stats[author][date]["files_changed"] += commit["files_changed"]
 	stats[author][date]["lines_changed"] += commit["insertions"] + commit["deletions"]
 	stats[author][date]["insertions"] += commit["insertions"]
 	stats[author][date]["deletions"] += commit["deletions"]
-	stats[author][date]["normalized_changes"] += round(stats[author][date]["lines_changed"] / (commit["files_changed"]+1))
-	sum_changes += stats[author][date]["lines_changed"]
-	sum_norm += stats[author][date]["normalized_changes"]
-	num_days = len(stats[author].keys())
-	stats[author][date]["avg_changes"] = round(sum_changes / num_days)
-	stats[author][date]["avg_normalized_changes"] = round(sum_norm / num_days)
+	stats[author][date]["normalized_changes"] = round(stats[author][date]["lines_changed"] / (commit["files_changed"]+1)) * 25
+	#print(stats[author][date]["normalized_changes"])
+
+for author in stats.keys():
+	sum_lines = 0
+	sum_norm = 0
+	
+	for date_index, date in enumerate(sorted(stats[author].keys())):
+		if "avg_lines" not in stats[author][date]:
+			stats[author][date]["avg_lines"] = 0
+		if "avg_norm" not in stats[author][date]:
+			stats[author][date]["avg_norm"] = 0
+		sum_lines += stats[author][date]["lines_changed"]
+		sum_norm += stats[author][date]["normalized_changes"]
+		stats[author][date]["avg_norm"] = sum_norm / (date_index+1)
+		stats[author][date]["avg_lines"] = sum_lines / (date_index+1)
+		#print(sum_norm, (date_index+1), sum_norm / (date_index+1), stats[author][date]["normalized_changes"])
+
+for author in sorted(stats.keys()):
+	dates = sorted(list(stats[author].keys()))
+	plt.bar(dates, list(map(lambda x: stats[author][x]["lines_changed"], dates)))
+	plt.plot(dates, list(map(lambda x: stats[author][x]["avg_lines"], dates)), "r")
+	plt.plot(dates, list(map(lambda x: stats[author][x]["avg_norm"], dates)), "g")
+	#plt.bar(dates, list(map(lambda x: stats[author][x]["normalized_changes"], dates)))
+	#plt.ylabel('some numbers')
+	plt.xlabel('dates')
+	plt.show()
+
 
 with open("res.csv", "w") as r:
 	title_printed = False
@@ -85,12 +105,12 @@ with open("res.csv", "w") as r:
 		for date in sorted(stats[author].keys()):
 			if not title_printed:
 				keys = stats[author][date].keys()
-				r.write(("author,date"+(",{}"*len(keys))+"\n").format(*keys))
+				r.write(("author,date"+(",\t{}"*len(keys))+"\n").format(*keys))
 				title_printed = True
-			res = (",{}"*(len(keys)+2)+"\n")[1:].format(
-					author,
-					date, 
-					*map(lambda x: stats[author][date][x], stats[author][date].keys())
+			res = (",{}\t"*(len(keys)+2)+"\n")[1:].format(
+					author.split("@")[0],
+					date.strftime("%d-%m-%y"), 
+					*map(lambda x: round(stats[author][date][x]), stats[author][date].keys())
 				)
 			#print(res)
 			r.write(res)
